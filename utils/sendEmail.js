25af0d05e8cd6f3609b8d6979f55e0ed52e4fefd
hardikrana929 <hardikrana929@gmail.com>
2026-06-01 16:21:08 +0530

    add reset password logic

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -13,13 +13,31 @@ const sendEmail = async (email, otp) => {
         const maileOption = {
             from: process.env.MYEMAIL,
             to: email,
-            subject: "EduValut Password Reset OTP",
+            subject: "EduVault Password Reset OTP",
+
             html: `
-                <div>
-                    <h2> EduValut Password Reste </h2>
-                    <p> Your OTP is: </p>
-                    <h1> ${otp} </h1>
-                    <p>This OTP will expire in 5 minutes.</p>
+                <div style="font-family: Arial, sans-serif; padding: 20px; text-align: center;">
+
+                    <img 
+                        src="https://res.cloudinary.com/ddn203hk8/image/upload/v1780300987/Gemini_Generated_Image_eb6jjieb6jjieb6j_iirm3r.png" 
+                        alt="EduVault Logo"
+                        style="width: 500px; margin-bottom: 20px;"
+                    />
+                    <h2 style="color: #2563eb;">
+                        EduVault Password Reset
+                    </h2>
+                    <p style="font-size: 16px; color: #555;">
+                        Your OTP for password reset is:
+                    </p>
+                    <h1 style="letter-spacing: 5px; color: #111;">
+                        ${otp}
+                    </h1>
+                    <p style="color: #777; margin-top: 20px;">
+                        This OTP will expire in 5 minutes.
+                    </p>
+                    <p style="color: red; font-size: 14px;">
+                        Do not share this OTP with anyone.
+                    </p>
                 </div>
             `,
         };
