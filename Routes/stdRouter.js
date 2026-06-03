ea1804c328b4459dc8dd1cef98eb13fda39270c7
hardikrana929 <hardikrana929@gmail.com>
2026-06-03 13:19:55 +0530

    debug login

--- Routes/stdRouter.js
+++ Routes/stdRouter.js
@@ -12,6 +12,11 @@ router.post("/signup-student", stdSignup);
 
 router.post("/signup-admin", adminSignup);
 
-router.post("/login-student", studentLogin);
+// router.post("/login-student", studentLogin);
+router.post("/login-student", (req, res, next) => {
+  console.log("LOGIN ROUTE HIT");
+  console.log(req.body);
+  next();
+}, studentLogin);
 
 module.exports = router;
